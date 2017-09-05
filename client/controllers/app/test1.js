1a0f81f1ef13fec3404ee2ecf9ac410f8c6e196a
Oscar Meza <oscar.meza@flex.com>
2017-09-05 09:31:07 -0700

    [Studio] Add screen "test1"

---
+++ client/controllers/app/test1.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_test1', app_test1);
+function app_test1($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.test1', {
+        views: {
+            app: {
+                controller: 'app_test1',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.test1');
+                }
+            }
+        }
     });
 });
